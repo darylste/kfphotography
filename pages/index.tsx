8be9da6cb76f762f360cb5386b9eea73d66224b8
Daryl Stensland <daryl.stensland13@icloud.com>
2022-04-14 21:54:01 +0100

    mising file:P

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
 import NavItem from '../atomic/atoms/NavItem/NavItem.component';
 import Nav from '../atomic/molecules/Nav/Nav.component';
