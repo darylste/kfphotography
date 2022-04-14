041dffd8ffcc731474afdc5e5aee629285163d49
Daryl Stensland <daryl.stensland13@icloud.com>
2022-04-14 22:14:42 +0100

    header component refactored

--- atomic/molecules/Nav/Nav.component.tsx
+++ atomic/molecules/Nav/Nav.component.tsx
@@ -1,11 +1,16 @@
+import NavItem from '../../atoms/NavItem/NavItem.component';
 import styles from './Nav.module.scss';
 
-interface INavProps {
-  children?: React.ReactNode;
-}
-
-const Nav: React.FC<INavProps> = ({ children }) => {
-  return <ul className={styles.nav}>{children}</ul>;
+const Nav: React.FC = () => {
+  return (
+    <ul className={styles.nav}>
+      <NavItem isActive>HOME</NavItem>
+      <NavItem>ABOUT</NavItem>
+      <NavItem>SERVICES</NavItem>
+      <NavItem>GALLERY</NavItem>
+      <NavItem>CONTACT</NavItem>
+    </ul>
+  );
 };
 
 export default Nav;
