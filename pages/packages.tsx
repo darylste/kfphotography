c3f9544272fe20dd3e6aedd6113555b79d30dab3
Daryl Stensland <daryl.stensland13@icloud.com>
2022-04-19 23:32:51 +0100

    removed unused imports

--- pages/packages.tsx
+++ pages/packages.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 
-import Footer from '../atomic/organisms/Footer/Footer.component';
 import Header from '../atomic/organisms/Header/Header.component';
 
 const Packages: NextPage = () => {
