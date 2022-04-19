1afd120dfb7ab0563fd8539163e909d47f1e4dba
Daryl Stensland <daryl.stensland13@icloud.com>
2022-04-19 23:30:28 +0100

    packages page created

---
+++ pages/packages.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+
+import Footer from '../atomic/organisms/Footer/Footer.component';
+import Header from '../atomic/organisms/Header/Header.component';
+
+const Packages: NextPage = () => {
+  return (
+    <>
+      <Header />
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default Packages;
